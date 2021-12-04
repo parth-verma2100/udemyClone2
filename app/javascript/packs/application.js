dbff8feee23ffa1ed967369535d63a2c186396d0
Parth Verma <parthverma011@gmail.com>
2021-12-04 10:17:07 +0530

    minor fixes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,37 +7,6 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("channels")
-
-
-//Dependencias globales
-import "bootstrap"
-import "../globals/off-canvas"
-import "../globals/hoverable-collapse"
-import "../globals/misc"
-import "../globals/settings"
-
-
-
-// Componentes externos
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css"
-import "jquery-steps/build/jquery.steps"
-import "jquery-validation/dist/jquery.validate"
-
-//Componentes de proleague
-
-// JS especifico para ciertas páginas
-import "../pages/wizard-forms"
-import "../pages/datepicker"
-import "../pages/form-validation"
-//= require jquery.validate.additional-methods 
-//= require jquery.validate
-//= require jquery
-//= require jquery_ujs
-
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
