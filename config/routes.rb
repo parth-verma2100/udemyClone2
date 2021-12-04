69a1a506987396644532521cfede13c4a84acfde
Parth Verma <parthverma011@gmail.com>
2021-12-04 11:02:35 +0530

    minor fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,17 @@
 Rails.application.routes.draw do
   get 'carts/show'
-  # devise_for :users
-  # devise_for :instructors
+  devise_for :users
+  devise_for :instructors
   resources :enrollments
   resources :order_items
   resources :carts 
   resources :courses do
    resources :videos
   end
-  
   root 'courses#home'
   get '/courses/my_enroll/:id', to: 'courses#create', as:'enroll_regist'
   get '/courses/enroll/:id', to: 'enrollments#create', as:'enroll_course'
   get 'cart/checkout', to:'carts#checkout', as:'checkout'
   post 'courses/add_to_cart/:id', to:'courses#add_to_cart', as: 'add_to_cart'
   delete 'courses/remove_from_cart/:id', to:'courses#remove_from_cart', as:'remove_from_cart'
-  
-# root 'products#index'
-
-  # get 'carts/:id' => "carts#show", as: "cart"
-  # delete 'carts/:id' => "carts#destroy"
-
-# post 'line_items/:id/add' => "line_items#add_quantity", as: "line_item_add"
-# post 'line_items/:id/reduce' => "line_items#reduce_quantity", as: "line_item_reduce"
-# post 'line_items' => "line_items#create"
-# get 'line_items/:id' => "line_items#show", as: "line_item"
-# delete 'line_items/:id' => "line_items#destroy"
-
-# resources :products
-# resources :orders
 end
